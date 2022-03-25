b18560db11012b8f6a8c27c68ef8f26715efa687
Rian Carter <R.Carter86@hotmail.com>
2022-03-25 10:44:27 -0700

    add src directory containing css directory, styles.css, currencyExchange.js, index.html, and index.js

---
+++ src/js/currencyExchange.js
@@ -0,0 +1,15 @@
+export default class CurrencyExchange {
+  static async exchangeCurrency(selectedCurrency) {
+    try {
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${selectedCurrency}`;
+      console.log(url);
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
+  }
+}
