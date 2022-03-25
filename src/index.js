dac2f8bd0ff70bcdb06a878e48ff8b278caa04ab
Rian Carter <R.Carter86@hotmail.com>
2022-03-25 11:02:47 -0700

    start adding getElements function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/css/bootstrap.min.css';
 import './css/styles/css';
-import CurrencyExchange from './currencyExchange.js';
+import CurrencyExchange from './../src/js/currencyExchange.js';
 
+function getElements(response, userInput, selectedCurrency) {
+  if (response) {
+    const convertUSD = (response.conversion_rate * userInput).toFixed(2);
+    $('.showConverted').append(`USD is woth ${convertUSD} in ${selectedCurrency}.`);
+  } else {
+    $(`.showErrors`).text(`There was an error: ${response}`);
+  }
+}
