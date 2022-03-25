2c91a8ad650d74e4597f39f414d5f1c84616602a
Rian Carter <R.Carter86@hotmail.com>
2022-03-25 13:30:06 -0700

    WIP

--- src/js/currencyExchange.js
+++ src/js/currencyExchange.js
@@ -2,7 +2,7 @@ export default class CurrencyExchange {
   static async exchangeCurrency(selectedCurrency) {
     try {
       const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${selectedCurrency}`;
-      console.log(url);
+      // console.log(url);
       const response = await fetch(url);
       if (!response.ok) {
         throw Error(response.statusText);
