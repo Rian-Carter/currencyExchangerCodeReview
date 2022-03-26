28e5c2d4e3818989f2af37105557a5cd88f538b2
Rian Carter <R.Carter86@hotmail.com>
2022-03-25 21:16:20 -0700

    remove all console logs

--- src/js/currencyExchange.js
+++ src/js/currencyExchange.js
@@ -2,7 +2,6 @@ export default class CurrencyExchange {
   static async exchangeCurrency(selectedCurrency) {
     try {
       const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${selectedCurrency}`;
-      // console.log(url);
       const response = await fetch(url);
       if (!response.ok) {
         throw Error(response.statusText);
