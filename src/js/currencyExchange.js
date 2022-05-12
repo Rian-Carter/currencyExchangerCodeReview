5d2f027009e6908247036bb666e868ae49b440d1
Rian Carter <R.Carter86@hotmail.com>
2022-05-12 07:57:43 -0700

    fix error clear issue after real currency is selected

--- src/js/currencyExchange.js
+++ src/js/currencyExchange.js
@@ -1,7 +1,7 @@
 export default class CurrencyExchange {
-  static async exchangeCurrency(selectedCurrency) {
+  static async exchangeCurrency(startingCurrency, convertedCurrency) {
     try {
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${selectedCurrency}`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${startingCurrency}/${convertedCurrency}`;
       const response = await fetch(url);
       if (!response.ok) {
         throw Error(response.statusText);
